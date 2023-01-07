d3113785e3679561d84474d1e878cb9bca8aef36
siva prasad <sivaprasadreddytalapagala@gmail.com>
2023-01-07 20:26:28 +0530

    home-page-creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
 import { AdminComponent } from './admin/admin.component';
 import { SigninComponent } from './admin/signin/signin.component';
 import { SignupComponent } from './admin/signup/signup.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { HousedetailsComponent } from './owner/housedetails/housedetails.component';
 import { OwnerComponent } from './owner/owner.component';
 import { OwnersigninComponent } from './owner/ownersignin/ownersignin.component';
@@ -18,6 +21,9 @@ import { UsersignupComponent } from './user/usersignup/usersignup.component';
 const routes: Routes = [
   {path:'',component:UsersigninComponent,pathMatch: 'full'},
   {path:'home',component:HomepageComponent},
+  {path:'contact-us',component:ContactUsComponent},
+  {path:'about-us',component:AboutUsComponent},
+  {path:'how-it-works',component:HowItWorksComponent},
   {path:'owner',component:OwnerComponent,children:[
     {path:'',component:OwnersigninComponent},
     {path:'ownersignin',component:OwnersigninComponent},
